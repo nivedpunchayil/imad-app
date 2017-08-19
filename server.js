6a0b4f76ba64190c8fc45af42cfbe8af7ce8eeed
nivedpunchayil <31069050+nivedpunchayil@users.noreply.github.com>
2017-08-20 00:20:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/ui/madii.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui/madi.png'));
+  res.sendFile(path.join(__dirname, 'ui/madii.png'));
 });
 
 
