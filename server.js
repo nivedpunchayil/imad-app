6f0f384d5ecb4f3cf7c3613d5c703c26a5ba9da7
nivedpunchayil <31069050+nivedpunchayil@users.noreply.github.com>
2017-08-20 01:04:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 app.get('/input1',function (req, res)
 { res.send('thomachan is oola');});
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+  res.sendFile(path.join(__dirname, 'ui','style.css'));
 });
 
 app.get('/ui/small.png', function (req, res) {
