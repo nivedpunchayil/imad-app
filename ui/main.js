d1b6000b7ac008dcb0c5dc5382909800073ee768
nivedpunchayil <31069050+nivedpunchayil@users.noreply.github.com>
2017-08-23 21:36:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('Loaded!');
 var element=document.getElementById("main-text");
-element.innerHTML='new value'
+element.innerHTML='new value';
+var element=document.getElementById('wow');
+image.onclick=function(){
+    img.stylemarginleft='100px';
+};
